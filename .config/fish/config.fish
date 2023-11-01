2304f47723ec67ea5e063c985c3b5a59c565911c
cuibo <1956699404@qq.com>
2023-11-01 22:50:40 +0800

    feat:change on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,8 +18,9 @@ alias cd='z'
 alias pi='pip install -i https://pypi.tuna.tsinghua.edu.cn/simple'
 alias cp='xcp'
 alias vi='nvim'
-alias vim='nvim'
 alias to='tokei'
+alias cns='sudo rm -r ~/.local/state/nvim/'
+alias myclang='cp ~/.config/.clang-format ./'
 set -x FZF_DEFAULT_OPTS "--color=hl:#f391a9,fg:#afdfe4,hl+:#f391a9,fg+:#ffe600"
 set fish_color_command green --bold
 set fish_color_autosuggestion f47920
@@ -29,3 +30,12 @@ if test -f /home/cuibo/miniconda3/bin/conda
     eval /home/cuibo/miniconda3/bin/conda "shell.fish" hook $argv | source
 end
 # <<< conda initialize <<<
+
+# >>> JVM installed by coursier >>>
+# set -gx JAVA_HOME "/home/cuibo/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.20.1%252B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.20.1_1.tar.gz/jdk-11.0.20.1+1"
+# set -gx PATH "$PATH:/home/cuibo/.cache/coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.20.1%252B1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.20.1_1.tar.gz/jdk-11.0.20.1+1/bin"
+# <<< JVM installed by coursier <<<
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/cuibo/.local/share/coursier/bin"
+# <<< coursier install directory <<<
